a7de4cc70ecb19ccfe1335528bd91f35a1ebfde1
yinchuandong <yincd520@sina.com>
2015-11-12 00:25:21 +1100

    complete max heap sort

--- cpp/mediumheap.cpp
+++ cpp/mediumheap.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
-#include <string>
-#include <sstream>
-#include <queue>
 #include <vector>
 
 using namespace std;
 
 
+
 class MedianFinder {
 public:
 
-    void swap(int arr[], int i, int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    template <class T>
+    void swap(T &a, T &b){
+        T temp(a);
+        a = b;
+        b = temp;
     }
 
-    void adjustMinHeap(int arr[], int size, int i){
+    template <class T>
+    void adjustMinHeap(vector<T> &arr, int size, int i){
         int left, right, max;
         max = i;
         while(2*i < size){
@@ -30,14 +30,16 @@ public:
                 max = right;
             }
             if(max != i){
-                swap(arr, i, max);
+                swap(arr[i], arr[max]);
                 i = max;
             }else{
                 break;
             }
         }
     }
-    void adjustMaxHeap(int arr[], int size, int i){
+
+    template <class T>
+    void adjustMaxHeap(vector<T> &arr, int size, int i){
         int left, right, max;
         max = i;
         while(2*i < size){
@@ -51,7 +53,7 @@ public:
                 max = right;
             }
             if(max != i){
-                swap(arr, i, max);
+                swap(arr[i], arr[max]);
                 i = max;
             }else{
                 break;
@@ -59,24 +61,33 @@ public:
         }
     }
 
-    void sortMinHeap(int arr[], int size){
+    template <class T>
+    void sortMinHeap(vector<T> &arr, int size){
         for(int i = size - 1; i > 0; i--){
-            swap(arr, 0, i);
+            swap(arr[i], arr[0]);
             adjustMinHeap(arr, i, 0);
         }
     }
-    void sortMaxHeap(int arr[], int size){
+
+    template <class T>
+    void sortMaxHeap(vector<T> &arr, int size){
         for(int i = size - 1; i > 0; i--){
-            swap(arr, 0, i);
+            swap(arr[i], arr[0]);
             adjustMaxHeap(arr, i, 0);
         }
     }
 
-    void buildMinHeap(){
-        int size = 10;
-        int arr[] = {45,36,18,53,72,30,48,93,15,35};
-        //build init heap
-        
+    template <class T>
+    void test(vector<T> &arr){
+        swap(arr[0], arr[1]);
+    }
+
+    void buildMinHeap(){        
+        cout << "------buildMinHeap function---------------"<<endl;
+        vector<int> &arr = *assignVector();
+        int size = arr.size();
+
+        adjustMinHeap(arr, size, size/2);
         // printMat(arr, size);
         for (int i = size / 2; i >= 0; i--){
             adjustMinHeap(arr, size, i);
@@ -87,10 +98,11 @@ public:
     }
     
     void buildMaxHeap(){
-        int size = 10;
-        int arr[] = {45,36,18,53,72,30,48,93,15,35};
-        //build init heap
-        
+        cout << "------buildMaxHeap function---------------"<<endl;
+        vector<int> &arr = *assignVector();
+        int size = arr.size();
+
+        adjustMinHeap(arr, size, size/2);
         // printMat(arr, size);
         for (int i = size / 2; i >= 0; i--){
             adjustMaxHeap(arr, size, i);
@@ -100,8 +112,20 @@ public:
         printMat(arr, size);
     }
 
+    vector<int>* assignVector(){
+        int size = 10;
+        int originArr[] = {45,36,18,53,72,30,48,93,15,35};
+        vector<int> *arr = new vector<int>();
+        for(int i = 0; i < size; i++){
+            arr->push_back(originArr[i]);
+        }
 
-    void printMat(int arr[], int size){
+        return arr;
+    }
+
+    template <class T>
+    void printMat(vector<T> arr, int size){
+        // swap(arr[0], arr[1]);
         for(int i = 0; i < size; i++){
             cout<<arr[i]<< ' ';
         }
@@ -127,6 +151,7 @@ public:
 
 int main(int argNum, char* args[]){
     MedianFinder finder;
+
     finder.buildMinHeap();
     cout << "----------" << endl;
     finder.buildMaxHeap();
