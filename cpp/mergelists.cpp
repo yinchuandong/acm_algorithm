aee58e8c2b3d31041d22c84b89f4cec01f16abe0
yinchuandong <yincd520@sina.com>
2016-05-14 13:23:17 +1000

    modified the c++ version of mergelists

--- cpp/mergelists.cpp
+++ cpp/mergelists.cpp
@@ -3,48 +3,43 @@
 
 using namespace std;
 
-void swap(int &a, int &b){
-	int t = a;
+typedef struct tagNode{
+	int value;
+	int idInArray;
+	int idInList;
+}Node;
+
+void swap(Node &a, Node &b){
+	Node t = a;
 	a = b;
 	b = t;
 }
 
-
-void adjustHeap(int arr[], int size, int i){
-	int left, right, max;
-	max = i;
-	while(2*i < size){
-		left = 2*i + 1;
-		right = 2*i + 2;
-		if (left < size && arr[left] > arr[max]){
-			max = left;
-		}
-		if (right < size && arr[right] > arr[max]){
-			max = right;
-		}
-		if (max != i){
-			swap(arr[max], arr[i]);
-			i = max;
-		}else{
+void bottomUp(Node heap[], int size){
+	int index = size - 1;
+	int parent = 0;
+	while(index > 0){
+		parent = (index - 1) / 2;
+		if (heap[index].value >= heap[parent].value){
 			break;
 		}
+		swap(heap[index], heap[parent]);
+		index = parent;
 	}
 }
 
-
-void buildHeap(int arr[], int size){
-	for (int i = size / 2; i >= 0; i--){
-		adjustHeap(arr, size, i);
-	}
+void inject(Node heap[], int &size, Node elem){
+	heap[size++] = elem;
+	bottomUp(heap, size);
 }
 
-void heapsort(int arr[], int size){
-	for (int i = size - 1; i > 0; i--){
-		swap(arr[i], arr[0]);
-		adjustHeap(arr, i, 0);
-	}
+Node eject(Node heap[], int &size){
+	swap(heap[0], heap[size-1]);
+	Node elem = heap[size-1];
+	size --;
+	bottomUp(heap, size);
+	return elem;
 }
-
 
 void display(int arr[], int size){
 	for (int i = 0; i < size; i++){
@@ -57,22 +52,35 @@ void display(int arr[], int size){
 int merge(int n_args, ...){
 	va_list ap;
 	va_start(ap, n_args);
-	int n = va_arg(ap, int);
-	int *flat_arr = new int[n];
-	int idx = 0;
-	for(int i = 2; i <= n_args; i+=2) {
+	int n = va_arg(ap, int); // total length
+	int k = va_arg(ap, int); // k arrays in list
+	int hsize = 0; //size of min heap
+	Node *heap = new Node[k]; // heap with size being k
+	int **src = new int*[k]; // the 2-d array of source list
+	int *srcSize = va_arg(ap, int*);; // the array recording the length of src[k]
+	for(int i = 4; i <= n_args; i++) {
 		int *a = va_arg(ap, int*);
-		int len = va_arg(ap, int);
-		for (int j = 0; j < len; j++){
-			flat_arr[idx++] = a[j];
-		}
-		display(a, len);
+		int idInList = i - 4;
+		src[idInList] = a;
+		Node elem;
+		elem.value = a[0]; elem.idInArray = 0; elem.idInList = idInList;
+		inject(heap, hsize, elem);
 	}
-	display(flat_arr, n);
-	buildHeap(flat_arr, n);
-	heapsort(flat_arr, n);
-	display(flat_arr, n);
+	int *arr = new int[n];
+	int idx = 0;
+	while(hsize != 0){
+		Node elem = eject(heap, hsize);
+		arr[idx++] = elem.value;
+		if(elem.idInArray < srcSize[elem.idInList] - 1){
+			Node next;
+			next.value = src[elem.idInList][elem.idInArray+1];
+			next.idInArray = elem.idInArray + 1;
+			next.idInList = elem.idInList;
+			inject(heap, hsize, next);
+		}
+	}
 	va_end(ap);
+	display(arr, n);
 	return 0;
 }
 
@@ -81,10 +89,11 @@ int main(int argNum, char* args[]){
 
 	int A[] = {1, 4, 7, 11};
 	int B[] = {2, 5, 8, 10};
-	int C[] = {3, 6, 9};
-	int A_len = 4, B_len = 4, C_len = 3;
-	int n = A_len + B_len + C_len;
-	merge(7, n, A, A_len, B, B_len, C, C_len);
+	int C[] = {3, 6, 9, 12};
+	int srcSize[] = {4, 4, 4};
+	int n = 12;
+	int k = 3;
+	merge(6, n, k, srcSize, A, B, C);
 	return 0;
 }
 
