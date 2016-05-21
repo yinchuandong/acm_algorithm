6fe9c4c68094d43edd0d22c92201a40b2e5162ef
yinchuandong <yincd520@sina.com>
2016-05-21 11:20:34 +1000

    recover the wrong quickselect

--- cpp/quickselect.cpp
+++ cpp/quickselect.cpp
@@ -48,13 +48,32 @@ int quickselect(int A[], int lo, int hi, int k){
 	}
 }
 
+// this implementation has a bug,
+// when input is [1, 2, 3, 4, 5, 6, 7, 8] and k 3 or 5
+int quickselect_old(int A[], int lo, int hi, int k){
+	int s = partition(A, lo, hi);
+	if(lo + k - 1 == s){
+		return A[s];
+	}else{
+		if(s > lo + k - 1){
+			return quickselect_old(A, lo, s - 1, k);
+		}else{
+			return quickselect_old(A, s + 1, hi, k - (s + 1));
+		}
+	}
+}
 
 
 int main(int argnum, char *args[]){
-	// int A[] = {39, 23, 12, 77, 48, 61, 55, 78};
+	// correct version
 	int A[] = {3, 2, 1, 4, 5, 6, 7, 8};
 	int rt = quickselect(A, 0, 7, 3);
 	cout<<"result:"<<rt<<endl;
+
+	// wrong version
+	// int rt2 = quickselect_old(A, 0, 7, 3);
+	// cout<<"result:"<<rt2<<endl;
+
 	// int B[] = {72, 29, 64, 86, 33, 89, 38, 32, 94, 42};
 	// int rt2 = quickselect(B, 0, 9, 5);
 	// cout<<"result:"<<rt2<<endl;
